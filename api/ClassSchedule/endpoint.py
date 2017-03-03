37999707c317ea6e9e32156a74a96f04b097dd19
janet <jskenmotsu@gmail.com>
2017-03-03 10:18:31 -0800

    adding api/class_instance endpoint temporarily with id call

--- api/ClassSchedule/endpoint.py
+++ api/ClassSchedule/endpoint.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from sqlalchemy.orm.exc import NoResultFound
 
 from api.Base.helper import to_json
 from server import app
@@ -14,8 +14,13 @@ def get_class_schedule():
     return result
 
 
-@app.route('/class_instance')
-def get_class_instance():
-    class_instance = ClassInstance.query.filter(ClassInstance.date == '05/07/2016').one()
-    class_instance_formatted = datetime.strftime(class_instance.date, '%a, %m/%d/%y')
-    return class_instance_formatted
+@app.route('/api/class_instance/<int:id>')
+def get_class_instance(id):
+    try:
+        class_instance = ClassInstance.query.filter(ClassInstance.id == id).one()
+    except NoResultFound:
+        class_instance = {}
+
+    class_instance = to_json(class_instance)
+
+    return class_instance
