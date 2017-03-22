1513aba7ca15a9e13555cbe916abe2c125433f22
janet <jskenmotsu@gmail.com>
2017-03-22 13:00:09 -0700

    adding index route to see if it helps the issue

--- api/__init__.py
+++ api/__init__.py
@@ -3,3 +3,4 @@ from server import app
 from .Student.endpoint import get_students, get_student_class_instance, get_student_class_schedule
 from .Coach.endpoint import get_coach
 from .ClassSchedule.endpoint import get_class_schedule, get_class_instance
+from .Base.endpoint import get_index
