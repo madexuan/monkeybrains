7cb6b5911ebe7a036d28776cd5e8adb8d4302ca9
janet <jskenmotsu@gmail.com>
2017-05-11 09:42:33 -0700

    add api/process_login endpoint that succeeds on email match

--- api/Coach/endpoint.py
+++ api/Coach/endpoint.py
@@ -1,3 +1,7 @@
+from flask import request
+from sqlalchemy.orm.exc import NoResultFound
+
+import json
 from server import app
 from .model import Coach
 
@@ -6,3 +10,23 @@ from .model import Coach
 def get_coach():
     coach_janet = Coach.query.filter(Coach.email == 'janet@example.com').one()
     return coach_janet.email
+
+
+@app.route('/api/process_login', methods=['POST'])
+def process_login():
+    email = request.form.get("email")
+    # password = request.form.get("password")
+
+    error_message = {'error': 'Email and Password not recognized. Please re-enter.'}
+
+    try:
+        coach = Coach.query.filter_by(email=email).one()
+        return json.dumps({'success': email})
+        # password_entered_hash = str(hash(coach.password_salt + password))
+        # if str(coach.password_hash) != password_entered_hash:
+        #     return json.dumps(error_message)
+        # else:
+        #     return json.dumps({'success': 'yay'})
+
+    except NoResultFound:
+        return json.dumps(error_message)
