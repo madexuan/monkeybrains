165d5a5000bed056dff456baeb1d10f2276654ca
janet <jskenmotsu@gmail.com>
2017-05-26 18:17:20 -0700

    Add register and reset_password endpoints: * updates process_login endpoint to authenticate password

--- api/Coach/endpoint.py
+++ api/Coach/endpoint.py
@@ -1,10 +1,12 @@
 from flask import request
 from sqlalchemy.orm.exc import NoResultFound
+import binascii
+import os
 
 import json
 from server import app
 from .model import Coach
-
+from api.database import db
 
 @app.route('/coach')
 def get_coach():
@@ -16,18 +18,68 @@ def get_coach():
 def process_login():
     response = request.get_json()
     email = response.get('email')
-    # password = response.get("password")
+    password = response.get('password')
 
     error_message = {'error': 'Email and Password not recognized. Please re-enter.'}
 
     try:
         coach = Coach.query.filter_by(email=email).one()
+        password_entered_hash = str(hash(coach.password_salt + password))
+        if str(coach.password_hash) != password_entered_hash:
+            return json.dumps(error_message)
+        else:
+            return json.dumps({'success': email})
+
+    except NoResultFound:
+        return json.dumps(error_message)
+
+
+@app.route('/api/register', methods=['POST'])
+def register():
+    response = request.get_json()
+    email = response.get('email')
+    password = response.get('password')
+    name_first = response.get('name_first')
+    name_last = response.get('name_last')
+    is_admin = response.get('is_admin')
+
+    salt = binascii.hexlify(os.urandom(7))
+    salt = salt.decode()
+
+    error_message = {'error': 'Registration failed'}
+
+    try:
+        new_coach = Coach(
+            email=email,
+            name_first=name_first,
+            name_last=name_last,
+            password_salt=salt,
+            password_hash=hash(salt + password),
+            is_admin=is_admin)
+        db.session.add(new_coach)
+        db.session.commit()
+
         return json.dumps({'success': email})
-        # password_entered_hash = str(hash(coach.password_salt + password))
-        # if str(coach.password_hash) != password_entered_hash:
-        #     return json.dumps(error_message)
-        # else:
-        #     return json.dumps({'success': 'yay'})
+    except:
+        return json.dumps(error_message)
+
+
+@app.route('/api/reset_password', methods=['POST'])
+def reset_password():
+    response = request.get_json()
+    email = response.get('email')
+    password = response.get('password')
+    salt = binascii.hexlify(os.urandom(7))
+    salt = salt.decode()
+
+    error_message = {'error': 'Reset password failed'}
+    try:
+        coach = Coach.query.filter_by(email=email).one()
+        coach.password_salt = salt
+        coach.password_hash = hash(salt + password)
+        db.session.commit()
+
+        return json.dumps({'success': email})
 
     except NoResultFound:
         return json.dumps(error_message)
--- api/Coach/endpoint_test.py
+++ api/Coach/endpoint_test.py
@@ -43,8 +43,51 @@ class CoachTestCase(unittest.TestCase):
                              )),
                              content_type='application/json')
 
-    def test_login(self):
-        login_success = self.login('janet@example.com', 'default')
+    def register(self, email, password, name_first, name_last, is_admin):
+        return self.app.post('/api/register',
+                             data=json.dumps(dict(
+                                email=email,
+                                password=password,
+                                name_first=name_first,
+                                name_last=name_last,
+                                is_admin=is_admin,
+                             )),
+                             content_type='application/json')
+
+    def reset_password(self, email, password):
+        return self.app.post('/api/reset_password',
+                             data=json.dumps(dict(
+                                email=email,
+                                password=password
+                             )),
+                             content_type='application/json')
+
+    def test_login_flow(self):
+        # REGISTER
+        # succeeds with new email and password combo
+        register_success = self.register('janet1@example.com',
+                                         'default',
+                                         'janet',
+                                         'last_name',
+                                         True)
+        assert b'janet1@example.com' in register_success.data
+
+        # fails if email already registered
+        register_fail = self.register('janet@example.com',
+                                      'default',
+                                      'janet',
+                                      'last_name',
+                                      True)
+        assert b'Registration failed' in register_fail.data
+
+        # RESET PASSWORD
+        reset_password_success = self.reset_password('janet@example.com', 'newpassword')
+        assert b'janet@example.com' in reset_password_success.data
+        reset_password_fail = self.reset_password('janet2@example.com', 'default')
+        assert b'Reset password failed' in reset_password_fail.data
+
+        # LOGIN
+        login_success = self.login('janet@example.com', 'newpassword')
         assert b'janet@example.com' in login_success.data
         login_fail = self.login('idontexist@example.com', 'hacker')
         assert b'Email and Password not recognized. Please re-enter' in login_fail.data
