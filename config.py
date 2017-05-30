5657fd437aebf1675c2c9fd3eac5e6784c43fc0d
janet <jskenmotsu@gmail.com>
2017-05-30 10:52:13 -0700

    organizing config.py

--- config.py
+++ config.py
@@ -2,6 +2,8 @@ import os
 
 # flask
 SECRET_KEY = os.environ.get('FLASK_SECRET_KEY', default="fillthisin")
+DEBUG = os.environ.get('FLASK_DEBUG', default=False)  # always set debug to false in production
+
+# sqlalchemy
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', default='postgresql://localhost/monkeys')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-DEBUG = os.environ.get('FLASK_DEBUG', default=False)  # always set debug to false in production
