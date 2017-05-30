3a62ee37b10bbade0e76ba2d300b8d23ab26d721
janet <jskenmotsu@gmail.com>
2017-05-30 10:53:14 -0700

    install Flask-Login 0.4.0

--- api/__init__.py
+++ api/__init__.py
@@ -1,5 +1,5 @@
 from .database import connect_db
-from server import app
+from server import app, login_manager
 from .Student.endpoint import get_students, get_student_class_instance, get_student_class_schedule
 from .Coach.endpoint import get_coach
 from .ClassSchedule.endpoint import get_class_schedule, get_class_instance
--- server.py
+++ server.py
@@ -1,11 +1,8 @@
 from flask import Flask
+import flask_login
 
 
 app = Flask(__name__)
 app.config.from_object('config')
-
-
-# import from api package at the end of the file here to
-# circumvent circular import issues
-# http://flask.pocoo.org/docs/0.11/patterns/packages/#larger-applications
-# import api.views
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
