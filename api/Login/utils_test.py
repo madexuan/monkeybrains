5525ab760ed98bb28342fb079906d7235e3999cf
janet <jskenmotsu@gmail.com>
2017-06-21 09:27:43 -0700

    Installing SendGrid for email server:
    * uninstalling Flask-Mail

--- api/Login/utils_test.py
+++ api/Login/utils_test.py
@@ -1,11 +1,11 @@
 from .utils import send_email, create_email
 from api.Base.fixtures import email, subject, html
-from flask_mail import Message
+from sendgrid.helpers.mail import Mail, Email, Content
 
 from unittest.mock import patch
 
 
-@patch('api.Login.utils.mail.send')
+@patch('api.Login.utils._send_email')
 def test_send_email(mock_mail_send):
     send_email(email, subject, html)
     assert mock_mail_send.called
@@ -13,13 +13,12 @@ def test_send_email(mock_mail_send):
 
 def test_create_email():
     test_email = create_email(email, subject, html)
-    expected = Message(
-        subject=subject,
-        html=html,
-        sender='jskenmotsu@gmail.com',
-        recipients=[email])
+    from_email = Email("jskenmotsu@gmail.com")
+    to_email = Email(email)
+    content = Content("text/html", html)
+    expected = Mail(from_email, subject, to_email, content)
 
-    assert test_email.html == expected.html
+    assert test_email.from_email.email == expected.from_email.email
+    assert test_email.personalizations[0].tos[0]['email'] == expected.personalizations[0].tos[0]['email']
+    assert test_email.contents[0].value == expected.contents[0].value
     assert test_email.subject == expected.subject
-    assert test_email.sender == expected.sender
-    assert test_email.recipients == expected.recipients
--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 from flask import Flask
 import flask_login
 from flask_bcrypt import Bcrypt
-from flask_mail import Mail
+import sendgrid
+import os
 
 
 app = Flask(__name__, template_folder='api/templates')
@@ -9,4 +10,6 @@ app.config.from_object('config')
 bcrypt = Bcrypt(app)
 login_manager = flask_login.LoginManager()
 login_manager.init_app(app)
-mail = Mail(app)
+
+# initialize sendgrid
+sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
